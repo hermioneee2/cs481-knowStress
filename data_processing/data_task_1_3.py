bd454fd095ce0b681ed32ee76b397d466f65a5fe
runnie0427 <riyuna0427@gmail.com>
2023-05-02 20:21:27 +0900

    modofied threashold 75m, clarifying app category(is doing)

--- data_processing/data_task_1_3.py
+++ data_processing/data_task_1_3.py
@@ -2,6 +2,7 @@ from data_handle import user_data_dict
 import glob
 import csv
 from geopy.geocoders import Nominatim
+from haversine import haversine
 
 user=int(input())
 #user uid를 입력하면 csv파일이 나오는 형식. data_processing 파일 안에 data라는 파일과 그 안에 데이터들이 있어야 작동.
@@ -13,6 +14,10 @@ def geocoding_reverse(lat, lng):
     print(address)
     return address
 #주소변환 하기에는 주어진 좌표가 정확하지 않은 편인 거 같아서, 다른 방법을 찾는 게 아니면 사용하지 않는 쪽이 될 것 같습니다.
+
+def distance(lat1, lon1, lat2, lon2):
+    return(haversine((lat1, lon1), (lat2, lon2)))
+
 try:
     user_data_by_time = dict()
     with open('data_processing/data/esm_data.csv') as esm:
@@ -29,6 +34,7 @@ try:
     file_list = glob.glob(f"data_processing/data/P{'0'*(user<1000)}{user}/LocationEntity*")
 
     loc_dict = dict()
+    loc_near_dict = dict()
     for file in file_list:
         file=file.replace('\\', '/')
         with open(file, encoding='UTF8') as f:
@@ -39,7 +45,14 @@ try:
                     lon, lat, alt=float(line[2]), float(line[3]), float(line[1])
                     # addr = geocoding_reverse(lat, lon)
                     addr = (lon, lat, alt)
-                    if addr not in loc_dict:loc_dict[addr]=[0,0]
+                    visited = False
+                    for lon1, lat1, alt1 in loc_dict:
+                        if distance(lat, lon, lat1, lon1)<0.075: #threshold: 75 meter
+                            loc_near_dict[addr]=(lon1, lat1, alt1)
+                            visited=True
+                            addr = (lon1, lat1, alt1)
+                            break
+                    if not visited:loc_dict[addr]=[0,0]
                     for timestamp in user_data_by_time:
                         if (timestamp-60)*1000 <= now_time <= (timestamp+60)*1000:
                             loc_dict[addr][0] += user_data_by_time[timestamp]
