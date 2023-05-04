74aad056f5d1951b1318e61b187d97acdd2ef4c9
윤하나 <hana000510@naver.com>
2023-05-04 17:21:57 +0900

    dashboard1 almost complete

--- src/dashboardContents/dashboard1.js
+++ src/dashboardContents/dashboard1.js
@@ -3,15 +3,27 @@ import styled from "styled-components";
 import { Layout } from "antd";
 import {theme} from '../styles/Theme'
 import StressCode from "../dashboardMinor/StressCode";
+
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import SquareIcon from '@mui/icons-material/Square';
+import DisabledByDefaultIcon from '@mui/icons-material/DisabledByDefault';
+
+import { getColor } from "../dashboardMinor/Heatmap";
 const {Content, Sider } = Layout;
 
 
 
 const Dashboard1 = () => {
 
-  const dailyValues = [0, 1, 3, 4, 5, 6, 5]
+  const dailyValues = [3, 3.2, 3.5, 3.7, 4, 4.9, 5]
+  // const dailyValues = [[3, Mon], [3.2, Tue], [3.5, 3.7, 4, 4.9, 5]
+  const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  // var ddValues = dailyValues.map(function(e, i) {
+  //   return [e, days[i]]
+  // })
+  const weeklyValues = 2.6
+  const hourlyValues = [[3, 3.2, 3.5, 3.7, 4, 4.9, 5, 3, 2, 2, 2, 2],[-1, 3.2, 3.5, 3.7, 4, 4.9, 5, -1, 2, 2, 2, 2],[-1, 3.2, 3.5, 3.7, 4, 4.9, 5, -1, 2, 2, 2, 2],[-1, 3.2, 3.5, 3.7, 4, 4.9, 5, -1, 2, 2, 2, 2],[-1, 3.2, 3.5, 3.7, 4, 4.9, 5, -1, 2, 2, 2, 2],[-1, 3.2, 3.5, 3.7, 4, 4.9, 5, -1, 2, 2, 2, 2],[-1, 3.2, 3.5, 3.7, 4, 4.9, 5, -1, 2, 2, 2, 2]]
+
   var titleStyle = {
     marginBottom: 10,
     // fontWeight: 'bold',
@@ -20,53 +32,90 @@ const Dashboard1 = () => {
   };
 
   var WeeklyText = {
-    color : theme.colors.stress3,
+    color : getColor(weeklyValues),
     fontSize: '40px',
     fontWeight: 'bold',
   }
 
+  // var axisLabel = {
+  //   display: 'flex',
+  //   flexDirection: 'column',
+  //   justifyContent: 'space-between',
+  // }
+
+  var xLabel = {
+    color: theme.colors.grayKS,
+    fontWeight: 'lighter',
+    fontSize: '11px',
+    width: 10,
+    paddingRight: 25,
+
+  }
+
+  var yLabel = {
+    // display: 'flex',
+    // alignItems: 'right',
+    color: theme.colors.grayKS,
+    fontWeight: 'lighter',
+    fontSize: '11px',
+    width: 10,
+    // textAlign: 'right',
+    paddingTop: 2,
+    marginTop: 5,
+    paddingRight: 35,
+    // marginLeft: 5,
+    // marginTop: -1,
+    // paddingTop: -1,
+  }
+
   const Daily = dailyValues.map((item) => (
     // <li key={item.id}>{item.name}</li>
-    <div style={{display: 'block'}}>
-      <SquareIcon sx={{color: theme.colors["stress" + item]}} />
+    <div key={item.id} style={{display: 'block'}}>
+      <SquareIcon sx={{color: getColor(item), margin:-0.3, fontSize:'30px'}} />
+      {/* <text style={yLabel}>{item[1]}</text> */}
     </div>
     
   ));
 
-  // const Daily = dailyValues.map((v, i) => (
-  //   <div key={i} className="row">
-  //     {v.map((value, j) => (
-  //       <div
-  //         key={j}
-  //         className="cell"
-  //         style={{
-  //           backgroundColor: colors[Math.floor((value / max) * colors.length)]
-  //         }}
-  //       />
-  //     ))}
-    // </div>
+  const Hourly = hourlyValues.map((v, i) => (
+    <div key={i} className="row" style={{display: 'flex'}}>
+      <text style={yLabel}>{days[i]}</text>
+      {v.map((value, j) => (
+        <div style={{display: 'block'}} key={j}>
+          {value == -1 ? <DisabledByDefaultIcon sx={{color: '#CDCDCD', margin:-0.3, fontSize:'30px'}}/> : <SquareIcon sx={{color: getColor(value), margin:-0.3, fontSize:'30px'}} /> }
+        </div>
+      ))}
+    </div>
+  ));
 
   return (
     <Layout style={{ background: '#ffffff'}}>
 
-      <Sider style={{ background: '#ffffff', paddingLeft:20}}>
-        {/* <TitleStyle>Weekly</TitleStyle> */}
+      <Sider width={130} style={{ background: '#ffffff'}}>
         <text style={titleStyle}>Weekly</text>
         <text style={WeeklyText}>4.3</text>
       </Sider>
 
-      <Sider style={{ background: '#ffffff', paddingLeft:20}}>
-        <TitleStyle>Daily</TitleStyle>
-        <div style={{display: 'block', marginTop: 10}}>
-        {Daily}
-          {/* <SquareIcon sx={{color: theme.colors.stress3}} /> */}
-        </div>
+      <Sider width={60} style={{ background: '#ffffff'}}>
+        <text style={titleStyle}>Daily</text>
+        <div style={{display: 'block', marginTop: 10}}>{Daily}</div>
       </Sider>
 
       <Content>
-        {/* <TitleStyle>Hourly</TitleStyle> */}
         <text style={titleStyle}>Hourly</text>
-        <SquareIcon sx={{color: theme.colors.stress3}} />
+        <div style={{display: 'block', marginTop: 10}}>{Hourly}</div>
+        <div style={{paddingLeft: 25}}>
+          <text style={xLabel}>10AM</text>
+          <text style={xLabel}>12PM</text>
+          <text style={xLabel}>2PM</text>
+          <text style={xLabel}>4PM</text>
+          <text style={xLabel}>6PM</text>
+          <text style={xLabel}>8PM</text>
+          <text style={xLabel}>10PM</text>
+        </div>
+        
+        
+        
       </Content>
 
       <Sider style={{ background: '#ffffff', paddingLeft:20}}>
@@ -90,12 +139,12 @@ const Dashboard1 = () => {
   );
 };
 
-const TitleStyle = styled.text`
-  marginBottom: 10;
-  fontSize: '15px';
-  display: 'block';
-`
-;
+// const TitleStyle = styled.text`
+//   marginBottom: 10;
+//   fontSize: '15px';
+//   display: 'block';
+// `
+// ;
 
 
 export default Dashboard1;
