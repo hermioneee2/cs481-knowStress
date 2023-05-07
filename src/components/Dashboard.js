06ede11d7113e5219f86753827632faa0fce1938
hermioneee2 <hermioneee2@gmail.com>
2023-05-07 22:53:33 +0900

    added icon for header

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,11 +2,24 @@ import React from "react";
 import styled from "styled-components";
 import { Layout } from "antd";
 import DashboardContentSwitcher from "./DashboardContentSwitcher";
+import { QuestionCircleOutlined } from "@ant-design/icons";
+import { theme } from "../styles/Theme";
 
 const Dashboard = ({ title, dashboardNum }) => {
+  const iconStyle = {
+    fontSize: "28px",
+    color: theme.colors.stress1,
+    marginRight: `10px`,
+  };
+
   return (
     <DashboardContainer>
-      <DashboardTitle>{title}</DashboardTitle>
+      <DashboardTitleWrapper>
+        {dashboardNum === 4 || dashboardNum === 5 ? (
+          <QuestionCircleOutlined style={iconStyle} />
+        ) : null}
+        <DashboardTitle>{title}</DashboardTitle>
+      </DashboardTitleWrapper>
       <DashboardContentSwitcher dashboardNum={dashboardNum} />
     </DashboardContainer>
   );
@@ -18,12 +31,16 @@ const DashboardContainer = styled(Layout.Content)`
   margin-right: 300px;
   margin-top: 20px;
   margin-bottom: 20px;
-  padding-top: 20px;
-  padding-bottom: 15px;
+  padding-top: 30px;
+  padding-bottom: 30px;
   padding-left: 34px;
   padding-right: 34px;
   border-radius: 20px;
   box-shadow: 0px 4px 10px 0 rgba(0, 0, 0, 0.1);
+`;
+
+const DashboardTitleWrapper = styled(Layout.Content)`
+  display: flex;
 `;
 
 const DashboardTitle = styled(Layout.Content)`
