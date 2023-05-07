2cf76ba37ba8e85dad731ccdbb68451f80579b74
runnie0427 <riyuna0427@gmail.com>
2023-05-07 22:46:40 +0900

    dashboard 5

--- src/dashboardContents/dashboard5.js
+++ src/dashboardContents/dashboard5.js
@@ -4,14 +4,65 @@ import { Layout } from "antd";
 
 const Dashboard5 = () => {
   return (
-    <div>
-      <div style={{ height: "500px" }}>Dashboard 5</div>
-    </div>
+    <ContentLayout>
+      <CategorySelectionLayout>
+        <StepWrapper>
+          <StepHeader>Step 1</StepHeader>
+          <StepTitle>Select Category</StepTitle>
+        </StepWrapper>
+        <div>Buttons here...</div>
+      </CategorySelectionLayout>
+      <ResultGraphLayout>
+        <StepWrapper>
+          <StepHeader>Step 2</StepHeader>
+          <StepTitle>View Stress Level Distribution</StepTitle>
+        </StepWrapper>
+        <div>Graph here...</div>
+      </ResultGraphLayout>
+    </ContentLayout>
   );
 };
 
-const ContentContainer = styled(Layout.Content)`
-  background-color: ${(props) => props.theme.colors.mainBgLightGray};
+const ContentLayout = styled.div`
+  display: flex;
+`;
+
+const CategorySelectionLayout = styled.div``;
+
+const ResultGraphLayout = styled.div`
+  margin-left: 60px;
+`;
+
+const StepWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 300px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+const StepHeader = styled.div`
+  width: 67px;
+  height: 29px;
+  font-family: "Open Sans";
+  font-size: 14px;
+  font-weight: 400;
+  margin-right: 15px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 13px;
+  padding-right: 13px;
+  border-radius: 15px;
+  color: ${(props) => props.theme.colors.grayKS};
+  background-color: ${(props) => props.theme.colors.selectedSidebarKS};
+`;
+
+const StepTitle = styled.div`
+  font-family: "Open Sans";
+  font-size: 15px;
+  font-weight: 400;
+  color: ${(props) => props.theme.colors.grayKS};
 `;
 
 export default Dashboard5;
