002c42c3a74a7fdf44f3e548f370420290f2c3c4
runnie0427 <riyuna0427@gmail.com>
2023-05-10 05:34:00 +0900

    solve dashboard4 problem

--- src/dashboardContents/dashboard4.js
+++ src/dashboardContents/dashboard4.js
@@ -1,8 +1,313 @@
-import React from "react";
+import React, { useState, useCallback, useRef } from "react";
 import styled from "styled-components";
-import { Layout } from "antd";
+import { theme } from "../styles/Theme";
+import { DownOutlined, EnvironmentFilled } from "@ant-design/icons";
+import { Slider, Collapse, theme as antdTheme } from "antd";
+import { GoogleMap, useLoadScript, OverlayView } from "@react-google-maps/api";
+
+import StressCodeBoxPlot from "../dashboardMinor/StressCodeBoxPlot";
+import BoxPlot from "../dashboardMinor/BoxPlot";
+
+// For sliders
+const age_marks = {
+  15: "15",
+  26: "me",
+  75: "75",
+};
+
+const app_usage_marks = {
+  100: "Least Usage",
+  26: "me",
+  1: "Most Usage",
+};
+
+const moved_distance_marks = {
+  100: "Least Active",
+  26: "me",
+  1: "Most Active",
+};
+
+const applyCommonMarkStyle = (marks) => {
+  for (const key in marks) {
+    if (Object.prototype.hasOwnProperty.call(marks, key)) {
+      marks[key] = {
+        style: commonMarkStyle,
+        label: marks[key],
+      };
+    }
+  }
+};
+
+const formatter = (value) => {
+  return `Top ${value}%`;
+};
+
+// // TODO: Scale this value
+// const locationRadiusFormatter = (value) => {
+//   return `Top ${value}%`;
+// };
+
+// textbox
+const { Panel } = Collapse;
+
+//map
+const mapContainerStyle = {
+  width: "300px",
+  height: "300px",
+  marginLeft: "100px",
+};
+
+const API_KEY = "";
 
 const Dashboard4 = () => {
+  //sliders: input values
+  const [ageRange, setAgeRange] = useState([15, 75]);
+  const [appUsageRange, setAppUsageRange] = useState([1, 100]);
+  const [movedDistanceRange, setMovedDistanceRange] = useState([1, 100]);
+  const [location, setLocation] = useState({
+    lat: 36.370053712983704,
+    lng: 127.3605726960359,
+  });
+  const [locationRadius, setLocationRadius] = useState(120);
+
+  const [myValue, setMyValue] = useState(4.3);
+
+  const [numOfPeople, setNumOfPeople] = useState(77);
+  const [topValue, setTopValue] = useState(20);
+  const [lowerQuartile, setLowerQuartile] = useState(2.3);
+  const [median, setMedian] = useState(3.8);
+  const [upperQuartile, setUpperQuartile] = useState(4);
+  const [min, setMin] = useState(1);
+  const [max, setMax] = useState(5.8);
+
+  //map
+  const mapRef = useRef(null);
+  const [address, setAddress] = useState("대한민국 대전광역시 한국과학기술원");
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: API_KEY,
+  });
+
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const getAddress = async (lat, lng) => {
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${API_KEY}`
+    );
+    const data = await response.json();
+    return data.results[0].formatted_address;
+  };
+
+  const onAfterChangeSlider = (value, sliderName) => {
+    switch (sliderName) {
+      case "age":
+        setAgeRange(value);
+        getBoxPlotData(
+          value,
+          appUsageRange,
+          movedDistanceRange,
+          location,
+          locationRadius
+        );
+        break;
+      case "appUsage":
+        setAppUsageRange(value);
+        getBoxPlotData(
+          ageRange,
+          value,
+          movedDistanceRange,
+          location,
+          locationRadius
+        );
+        break;
+      case "movedDistance":
+        setMovedDistanceRange(value);
+        getBoxPlotData(
+          ageRange,
+          appUsageRange,
+          value,
+          location,
+          locationRadius
+        );
+        break;
+      case "locationRadius":
+        setLocationRadius(value);
+        getBoxPlotData(
+          ageRange,
+          appUsageRange,
+          movedDistanceRange,
+          location,
+          value
+        );
+        break;
+      default:
+        break;
+    }
+  };
+
+  const getBoxPlotData = (
+    ageRange,
+    appUsageRange,
+    movedDistanceRange,
+    location,
+    locationRadius
+  ) => {
+    //get data...
+    const outputMyValue = 4.3;
+    const outputNumOfPeople = 60;
+    const outputTopValue = 19;
+    const outputLowerQuartile = 2.3;
+    const outputMedian = 3.3;
+    const outputUpperQuartile = 4.4;
+    const outputMin = 0;
+    const outputMax = 6;
+
+    setMyValue(outputMyValue);
+    setNumOfPeople(outputNumOfPeople);
+    setTopValue(outputTopValue);
+    setLowerQuartile(outputLowerQuartile);
+    setMedian(outputMedian);
+    setUpperQuartile(outputUpperQuartile);
+    setMin(outputMin);
+    setMax(outputMax);
+  };
+
+  //map
+  const onDragEnd = useCallback(async (event) => {
+    const center = mapRef.current.getCenter();
+
+    setLocation({
+      lat: center.lat(),
+      lng: center.lng(),
+    });
+
+    setAddress(await getAddress(center.lat(), center.lng()));
+  }, []);
+
+  const [overlayLoaded, setOverlayLoaded] = useState(false);
+
+  const onOverlayLoad = () => {
+    setOverlayLoaded(true);
+  };
+
+  const locationRangeCircleStyle = {
+    position: "absolute",
+    width: locationRadius * 2,
+    height: locationRadius * 2,
+    backgroundColor: theme.colors.selectionTransparent,
+    borderRadius: "50%",
+    textAlign: "center",
+    color: theme.colors.selectionBlue,
+    fontWeight: "bold",
+    fontSize: 16,
+    padding: 4,
+    border: `2px solid ${theme.colors.selectionBlue}`,
+    marginLeft: `-${locationRadius}px`,
+    marginTop: `-${locationRadius}px`,
+  };
+
+  const LocationRangeCircle = () => {
+    const ref = useRef(null);
+    return (
+      <OverlayView
+        position={location}
+        mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+        onLoad={onOverlayLoad}
+      >
+        {overlayLoaded && (
+          <div style={locationRangeCircleStyle} ref={ref}></div>
+        )}
+      </OverlayView>
+    );
+  };
+
+  const locationIconStyle = {
+    position: "absolute",
+    fontSize: "36px",
+    color: theme.colors.selectionBlue,
+    marginLeft: `-18px`,
+    marginTop: `-36px`,
+  };
+
+  const LocationPin = () => {
+    return (
+      <OverlayView
+        position={location}
+        mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+        onLoad={onOverlayLoad}
+      >
+        {overlayLoaded && <EnvironmentFilled style={locationIconStyle} />}
+      </OverlayView>
+    );
+  };
+
+  // text
+  const { token } = antdTheme.useToken();
+  const panelStyle = {
+    marginBottom: 15,
+    background: token.colorFillAlter,
+    borderRadius: token.borderRadiusLG,
+    border: "none",
+    fontFamily: "Open Sans",
+    fontSize: "12px",
+  };
+
+  const PeoplRangeExplanation = (
+    <>
+      Among{" "}
+      <span
+        style={{
+          fontWeight: 700,
+          color: theme.colors.selectionBlue,
+        }}
+      >
+        {numOfPeople}
+      </span>{" "}
+      people in the group
+    </>
+  );
+
+  const boxplotExplanation1 = (
+    <>
+      <span style={{ color: theme.colors.stress3 }}>
+        My average stress level of {myValue} ranked in top {topValue}% among{" "}
+        {numOfPeople} people in the group.
+      </span>{" "}
+      Top 1% indicates the most stressed and top 100% indicates the least
+      stressed in the group. The closer your ranking is to top 1%, the more
+      stressed you are compared to the group.
+    </>
+  );
+
+  const boxplotExplanation2 = (
+    <>
+      From top to bottom, each horizontal line indicates:
+      <ul style={{ listStyleType: "disc", marginLeft: "-15px" }}>
+        <li>100th percentiles: Maximum </li>
+        <li>75th percentiles: Third quartile (Q3)</li>
+        <li>50th percentiles: Median (Q2)</li>
+        <li>25th percentiles: First quartile (Q1)</li>
+        <li>0th percentiles: Minimum</li>
+      </ul>
+      This means that Q1 represents the value below which 25% of the data falls,
+      Q2 represents the value below which 50% of the data falls, etc. The box
+      represents the middle 50% of the data, between the first quartile (Q1) and
+      the third quartile (Q3).
+    </>
+  );
+
+  const MyRankingTextWrapper = styled.div`
+    display: flex;
+    z-index: 1;
+    position: absolute;
+    margin-left: 205px;
+    // margin-top: -25px;
+    // margin-top: 184px;
+    margin-top: ${((myValue * (184 + 25)) / 6) * -1 + 184}px;
+  `;
+
   return (
     <ContentLayout>
       <SliderLayout>
