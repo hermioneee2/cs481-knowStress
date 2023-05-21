274ae62adb434f2c02468ea4be290a6b20bfe9c7
hermioneee2 <hermioneee2@gmail.com>
2023-05-21 16:31:53 +0900

    Updates

---
+++ precache-manifest.52da51ad5a8ad5a5624384e093812455.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "a20769e30ef536fa43ae",
+    "url": "/cs481-knowstress-deploy/static/css/main.8f4d47ee.chunk.css"
+  },
+  {
+    "revision": "a20769e30ef536fa43ae",
+    "url": "/cs481-knowstress-deploy/static/js/main.2199ef66.chunk.js"
+  },
+  {
+    "revision": "52e12826e3d5b47aa271",
+    "url": "/cs481-knowstress-deploy/static/js/runtime~main.1697bef8.js"
+  },
+  {
+    "revision": "61b59a2bea327adc86c7",
+    "url": "/cs481-knowstress-deploy/static/js/2.0599e53d.chunk.js"
+  },
+  {
+    "revision": "18717c1259971dbf3d0f3877c5ad3e2b",
+    "url": "/cs481-knowstress-deploy/static/media/Bleacher Report.18717c12.png"
+  },
+  {
+    "revision": "bd33ce4512b2c04a7f33114b77a02b28",
+    "url": "/cs481-knowstress-deploy/static/media/Chrome.bd33ce45.png"
+  },
+  {
+    "revision": "aec0f52e1f6f651bfedd04155f609500",
+    "url": "/cs481-knowstress-deploy/static/media/Dropsync.aec0f52e.png"
+  },
+  {
+    "revision": "e8245567e298448687990514c65e36f8",
+    "url": "/cs481-knowstress-deploy/static/media/Instagram.e8245567.png"
+  },
+  {
+    "revision": "722084581132a56a168c5d40a24d75e0",
+    "url": "/cs481-knowstress-deploy/static/media/MLB9이닝스19.72208458.png"
+  },
+  {
+    "revision": "903fddfdc69dd3907a5ae6bc292ecf1d",
+    "url": "/cs481-knowstress-deploy/static/media/MTP.903fddfd.png"
+  },
+  {
+    "revision": "0199dbefadcd76700813973b33b3d0a8",
+    "url": "/cs481-knowstress-deploy/static/media/Melon.0199dbef.png"
+  },
+  {
+    "revision": "090daea33cd0d47dab065800d9e55334",
+    "url": "/cs481-knowstress-deploy/static/media/YouTube.090daea3.png"
+  },
+  {
+    "revision": "7b4388c4c84960e287fdd27e631b7ad2",
+    "url": "/cs481-knowstress-deploy/static/media/연락처.7b4388c4.png"
+  },
+  {
+    "revision": "314c41e4d0a356c09aba32af738727de",
+    "url": "/cs481-knowstress-deploy/static/media/카카오톡.314c41e4.png"
+  },
+  {
+    "revision": "3be6132e00801e4ed3b297b76cb1bb7c",
+    "url": "/cs481-knowstress-deploy/index.html"
+  }
+];
